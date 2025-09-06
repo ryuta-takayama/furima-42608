302be71def6e7886b69d1602a08b79195d823389
ryuta-takayama <tryuta9@gmail.com>
2025-09-06 17:43:33 +0900

    Itemモデルバリテーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,14 @@
 class Item < ApplicationRecord
-  
+  validates :item_name, presence: true
+  validates :item_explain, presence: true
+  validates :category_id, presence: true
+  validates :item_condition_id, presence: true
+  validates :shipping_fee_burden_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :shipping_lead_time_id, presence: true
+  validates :price, presence: true
+  validates :user_id, presence: true
+
+  belongs_to :user
+  has_one_attached :image
 end
