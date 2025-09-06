2ae926e21cd74f5c7b90ce553b5161dea21b9cdc
ryuta-takayama <tryuta9@gmail.com>
2025-09-07 02:12:09 +0900

    Active hashのバリテーション設定（id:1以外を選ぶ。1を選んだらエラーメッセージ表示）

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
+
   validates :item_name, presence: true
   validates :item_explain, presence: true
   validates :category_id, presence: true
@@ -12,11 +13,15 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-   extend ActiveHash::Associations::ActiveRecordExtensions
-    belongs_to :category
-    belongs_to :item_condition
-    belongs_to :shipping_fee_burden
-    belongs_to :prefecture
-    belongs_to :shipping_lead_time
-    
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :item_condition
+  belongs_to :shipping_fee_burden
+  belongs_to :prefecture
+  belongs_to :shipping_lead_time
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :item_condition_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_fee_burden_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_lead_time_id, numericality: { other_than: 1, message: "can't be blank" }
 end
