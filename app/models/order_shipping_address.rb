84cf5a84262e09d35a2e412a48d8de280d1a58a7
ryuta-takayama <tryuta9@gmail.com>
2025-09-15 03:50:22 +0900

    保存したいカラム名を属性値として扱えるよう設定（ゲッター・セッターメソッドを同時定義※決済情報除く）

--- app/models/order_shipping_address.rb
+++ app/models/order_shipping_address.rb
@@ -1,3 +1,4 @@
 class OrderShippingAddress 
   include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :user_id, :item_id, :order_id
 end
