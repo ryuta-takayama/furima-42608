e0226aed4997c7e15a039a1a6d9f7dbdde5ee7cd
ryuta-takayama <tryuta9@gmail.com>
2025-09-15 04:28:52 +0900

    formオブジェクトにバリデーションの処理及び単体モデルのバリテーション削除

--- app/models/order_shipping_address.rb
+++ app/models/order_shipping_address.rb
@@ -1,4 +1,16 @@
 class OrderShippingAddress 
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :user_id, :item_id, :order_id
+  attr_accessor :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :user_id, :item_id
+
+  with_options presence: true do
+    validates :postal_code, format: {with: /\A\d{3}[-]\d{4}\z/ }
+    validates :prefecture_id
+    validates :city
+    validates :address_line
+    validates :phone_number, format: {with: /\A\d{10,11}\z/}
+    validates :user_id
+    validates :item_id
+  end
+ extend ActiveHash::Associations::ActiveRecordExtensions
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 end
--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,12 +1,5 @@
 class ShippingAddress < ApplicationRecord
 
-
-  validates :postal_code, presence: :true, format: {with: /\A\d{3}[-]\d{4}\z/}
-  validates :prefecture_id, presence: :true
-  validates :city, presence: :true
-  validates :address_line, presence: :true
-  validates :phone_number, presence: :true, format: {with: /\A\d{10,11}\z/}
-
   belongs_to :order
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
