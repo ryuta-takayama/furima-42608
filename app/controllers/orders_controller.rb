6523736d19ec33ec830c910df483566cedbcccde
ryuta-takayama <tryuta9@gmail.com>
2025-09-15 02:21:51 +0900

    indexアクションの定義の記述（自身の出品または売り切れ商品の場合トップページに遷移）

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,8 +2,12 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index]
 
   def index
+    @item = Item.find(params[:item_id])
+    if @item.user_id == current_user.id || @item.order.present?
+      redirect_to root_path
+    end
   end
-  
+
 
 
 end
