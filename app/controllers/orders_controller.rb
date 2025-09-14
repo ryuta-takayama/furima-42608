db2ef8d85f0424bab8d97f075d7f5aec34ffba24
ryuta-takayama <tryuta9@gmail.com>
2025-09-15 02:38:12 +0900

    各モデルにorderとのアソシエーションの設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,6 +3,7 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
+    binding.pry
     if @item.user_id == current_user.id || @item.order.present?
       redirect_to root_path
     end
