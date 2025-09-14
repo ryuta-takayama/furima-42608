0913ac583c68bd157cfb8897fbc34cd3cdfdda96
ryuta-takayama <tryuta9@gmail.com>
2025-09-14 20:24:39 +0900

    マイグレーション作成

--- db/migrate/20250914100745_create_orders.rb
+++ db/migrate/20250914100745_create_orders.rb
@@ -2,6 +2,9 @@ class CreateOrders < ActiveRecord::Migration[7.1]
   def change
     create_table :orders do |t|
 
+      t.references :user, null: false, foreign_key: true
+      t.references :item, null: false, foreign_key: true, index: { unique: true }
+
       t.timestamps
     end
   end
--- db/migrate/20250914100928_create_shipping_addresses.rb
+++ db/migrate/20250914100928_create_shipping_addresses.rb
@@ -2,6 +2,14 @@ class CreateShippingAddresses < ActiveRecord::Migration[7.1]
   def change
     create_table :shipping_addresses do |t|
 
+      t.references :order, null: false, foreign_key: true, index: { unique: true }
+      t.string :postal_code, null: false
+      t.integer :prefecture_id, null: false
+      t.string :city, null: false
+      t.string :address_line, null: false
+      t.string :building_name
+      t.string :phone_number, null: false
+
       t.timestamps
     end
   end
