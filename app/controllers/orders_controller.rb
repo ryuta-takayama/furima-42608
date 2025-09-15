0b0ad89aaada0d3ebc5a15af13e2ab671c2cac1d
ryuta-takayama <tryuta9@gmail.com>
2025-09-16 06:01:20 +0900

    クレジット決済機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,6 +6,7 @@ class OrdersController < ApplicationController
     if @item.user_id == current_user.id || @item.order.present?
       redirect_to root_path
     end
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @order_shipping_address = OrderShippingAddress.new
 
   end
@@ -14,19 +15,30 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_shipping_address = OrderShippingAddress.new(order_params)
     if @order_shipping_address.valid?
+      pay_item
       @order_shipping_address.save
       redirect_to root_path
     else
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
       render :index, status: :unprocessable_entity
     end
   end
 
-  
+
 
   private
 
 
    def order_params
-    params.require(:order_shipping_address).permit(:postal_code, :prefecture_id, :city, :address_line, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_shipping_address).permit(:postal_code, :prefecture_id, :city, :address_line, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+   end
+
+   def pay_item
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: Item.find(order_params[:item_id]).price,
+        card: order_params[:token],
+        currency: 'jpy'
+      )
    end
 end
--- app/models/order_shipping_address.rb
+++ app/models/order_shipping_address.rb
@@ -1,6 +1,6 @@
 class OrderShippingAddress 
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :address_line, :building_name, :phone_number, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :city, :address_line, :building_name, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A\d{3}[-]\d{4}\z/ }
@@ -10,6 +10,7 @@ class OrderShippingAddress
     validates :phone_number, format: {with: /\A\d{10,11}\z/}
     validates :user_id
     validates :item_id
+    validates :token
   end
  extend ActiveHash::Associations::ActiveRecordExtensions
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
