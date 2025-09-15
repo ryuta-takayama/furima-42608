713848eea794e6b665b01569ac435a2f792cf763
ryuta-takayama <tryuta9@gmail.com>
2025-09-15 22:41:50 +0900

    formオブジェクトのcreateアクションの記述およびエラーメッセージ表示の設定

--- app/models/order_shipping_address.rb
+++ app/models/order_shipping_address.rb
@@ -1,6 +1,6 @@
 class OrderShippingAddress 
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :city, :address_line, :building_name, :phone_number, :user_id, :item_id
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A\d{3}[-]\d{4}\z/ }
@@ -16,7 +16,7 @@ class OrderShippingAddress
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
-    ShippingAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address_line: address, building_name: building_name, phone_number: phone_number, order_id: order.id)
+    ShippingAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address_line: address_line, building_name: building_name, phone_number: phone_number, order_id: order.id)
 
   end
 end
